6d7aacfa660df2f16ffbf221678913710edbdcc5
AnaisUrlichs <urlichsanais@gmail.com>
2021-09-11 11:29:08 +0100

    changes to the app, new version

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const storiesReducer = (state, action) => {
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
-    'Civo Cloud'
+    'Civo Kubernetes'
   )
 
   const [url, setUrl] = React.useState(
